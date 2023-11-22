3c9d3e1258be7b846b8ceb1d26fb0bbad31d25d9
SaumyaGupta-1907 <gupta.saumy@northeastern.edu>
2023-11-22 16:53:11 -0500

    updated bleu calculations

--- bleu.py
+++ bleu.py
@@ -4,19 +4,20 @@ import Preprocessing
 import test
 import numpy as np
 
-reference = Preprocessing.test_dataset.values()
+reference = Preprocessing.test_dataset
 bleu = []
 for img in tqdm(Preprocessing.test_dataset.keys()):
     caption = test.predict_captions(img).split()
-    bleu1 = sentence_bleu(reference, caption)
+    bleu1 = sentence_bleu(reference[img], caption)
     bleu.append(bleu1)
+    break
 
 print("Mean For Full Predicted Captions BLEU {:4.3f}".format(np.mean(bleu)))
 
 bleu = []
 for img in tqdm(Preprocessing.test_dataset.keys()):
     caption = test.beam_search_predictions(img, beam_index=5).split()
-    bleu1 = sentence_bleu(reference, caption)
+    bleu1 = sentence_bleu(reference[img], caption)
     bleu.append(bleu1)
 
 print("Mean For Beam Search Captions BLEU {:4.3f}".format(np.mean(bleu)))
